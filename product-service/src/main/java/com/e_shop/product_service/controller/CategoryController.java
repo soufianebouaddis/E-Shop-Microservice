8fe5e1e83e0e5eeef35302d9a8c79d981d9fe7d9
mjustatool <fousenaibdisbdis@gmail.com>
2024-11-24 19:33:26 +0100

    Compeleting logic controller of product-service

--- product-service/src/main/java/com/e_shop/product_service/controller/CategoryController.java
+++ product-service/src/main/java/com/e_shop/product_service/controller/CategoryController.java
@@ -1,4 +1,60 @@
 package com.e_shop.product_service.controller;
 
+import com.e_shop.product_service.dto.CategoryDTO;
+import com.e_shop.product_service.model.Category;
+import com.e_shop.product_service.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/category")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/create-category")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<?> creatcategory(@RequestBody CategoryDTO categoryDTO) {
+        try{
+            return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.save(categoryDTO));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+    @GetMapping("/get-categories")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<List<Category>> getcategorys() {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategories());
+    }
+    @GetMapping("/get-category/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<Category> getcategory(@PathVariable int id){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategory(id));
+    }
+    @GetMapping("/delete-category/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<?> deletecategory(@PathVariable int id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(categoryService.delete(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+    @GetMapping("/update-category/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<?> updatecategory(@PathVariable int id, @RequestBody CategoryDTO categoryDTO){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(categoryService.update(id,categoryDTO));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
 }
--- product-service/src/main/java/com/e_shop/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/e_shop/product_service/controller/ProductController.java
@@ -6,10 +6,7 @@ import com.e_shop.product_service.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,14 +22,14 @@ public class ProductController {
 
     @GetMapping("/create-product")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public ResponseEntity<?> creatProduct(ProductDTO productDTO) {
+    public ResponseEntity<?> creatProduct(@RequestBody ProductDTO productDTO) {
         try{
             return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(productDTO));
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
-    @GetMapping("/get-product")
+    @GetMapping("/get-products")
     @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
     public ResponseEntity<List<Product>> getProducts() {
         return ResponseEntity.status(HttpStatus.OK).body(productService.getProducts());
@@ -51,5 +48,13 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
-
+    @GetMapping("/update-product/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
+    public ResponseEntity<?> updateProduct(@PathVariable int id, @RequestBody ProductDTO productDTO){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(productService.update(id,productDTO));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
 }
